f089f76e9b4b06239e9f13fadf46381c879d3532
punk7890 <90301698+punk7890@users.noreply.github.com>
2023-09-09 23:02:16 -0700

    Update

--- src/GlobalPointer.asm
+++ src/GlobalPointer.asm
@@ -8,7 +8,7 @@
 .region 0x8000	;No idea how to properly use all 64kb of global pointer space with this compiler, so it's limited to 32kb.
 
 GLOBAL_POINTER:		;top of global pointer
-.asciiz "2023-09-09 16:33:17"
+.asciiz "2023-09-09 22:19:00"
 .align 4,0
 
 gDebugModeFlag:		;enables a set of test features
